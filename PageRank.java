7df506890365408e478cce00a9641036280f8295
david78k <root@c11node28.(none)>
2014-01-23 02:03:35 -0500

    update

--- PageRank.java
+++ PageRank.java
@@ -1,6 +1,51 @@
+// import Matrix
+import java.util.*;
+
 class PageRank {
+
+	// Matrix mat
+	//
+	PageRank() {
+
+	}
+
 
 	public static void main (String[] args) {
 		System.out.println("PageRank");
+		PageRank pr = new PageRank();
+		pr.start();
+	}
+	
+	void start() {
+		init();
+		parse();
+		rank();
+	}
+
+	/** initialize data
+	*/
+	void init() {
+		
+	}
+
+	void parse() {
+
+	}
+	
+	void rank() {
+
+	}
+
+	void mapreduce() {
+		map();
+		reduce();
+	}
+
+	void map() {
+
+	}
+
+	void reduce() {
+
 	}
 }
