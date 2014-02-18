1bd98870e90eac54023bc3a04d19f341fdc20eb7
david78k <root@c11node28.(none)>
2014-02-18 11:02:56 -0500

    update

--- p2/src/enron/outside_sent.sql
+++ p2/src/enron/outside_sent.sql
@@ -2,33 +2,30 @@
 -- and insert query results into a new table and a local file
 -- and display the results and the count
 
-set tname=enron_outside;
+set tname=enron_outside_sent;
 --set tname_origin=enron100;
-set tname_origin=enron;
+set tname_origin=enron_most_sent;
 
 -- create table 
 drop table ${hiveconf:tname};
 
 CREATE EXTERNAL TABLE ${hiveconf:tname} (
-  outsider STRING,
+  sender STRING,
   count INT
   );
 
 -- insert the people outside of enron with the number of correspondents
 -- order by frequency
 INSERT OVERWRITE TABLE ${hiveconf:tname}
---SELECT frome, collect_set(recip)
-SELECT frome, size(collect_set(recip)) freq
-FROM (
-	SELECT frome, trim(recipient) recip
-	FROM ${hiveconf:tname_origin}
-	LATERAL VIEW explode(split(toe, ',')) t AS recipient
-	WHERE (((NOT (frome like '%@enron.com%')) AND (recipient like '%@enron.com%'))
-                OR
-                ((frome like '%@enron.com%') AND (NOT (recipient like '%@enron.com%'))))
-) t1
-GROUP BY frome
-ORDER BY freq DESC
+SELECT frome, count
+FROM ${hiveconf:tname_origin}
+WHERE (NOT (frome like '%enron.com%')) 
+--WHERE ((NOT (frome like '%@enron.com%')) AND (recipient like '%@enron.com%'))
+      --          OR
+        --        ((frome like '%@enron.com%') AND (NOT (recipient like '%@enron.com%'))))
+--) t1
+--GROUP BY frome
+ORDER BY count DESC
 ;
 
 -- insert into a local file
