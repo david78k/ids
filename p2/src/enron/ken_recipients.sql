41f17ad17f5a17bfe8c79373811e13444d6f81ee
david78k <root@c11node28.(none)>
2014-02-19 00:23:51 -0500

    update

--- p2/src/enron/ken_recipients.sql
+++ p2/src/enron/ken_recipients.sql
@@ -2,7 +2,8 @@
 -- and insert query results into a new table and a local file
 -- and display the results and the count
 
-set tname=enron_ken_words;
+--set tname_origin=enron100;
+set tname=enron_ken_recipients;
 set tname_origin=enron_ken;
 
 -- create table 
@@ -16,13 +17,13 @@ CREATE EXTERNAL TABLE ${hiveconf:tname} (
 -- insert the records of ken lay
 -- order by frequency
 INSERT OVERWRITE TABLE ${hiveconf:tname}
-SELECT trim(word), count(1) freq
+SELECT frome, trim(recipient), count(1) freq
 FROM ${hiveconf:tname_origin}
-LATERAL VIEW explode(split(context, ' +')) t AS word
---WHERE frome != trim(recipient)
---	AND
---	((frome LIKE '%enron.com%') OR (recipient LIKE '%enron.com%'))
-GROUP BY trim(word)
+LATERAL VIEW explode(split(concat_ws(',', toe, cc), ',')) t AS recipient
+WHERE frome != trim(recipient)
+	AND
+	((frome LIKE '%enron.com%') OR (recipient LIKE '%enron.com%'))
+GROUP BY frome, trim(recipient)
 ORDER BY freq DESC
 ;
 
