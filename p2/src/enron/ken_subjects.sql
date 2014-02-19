7a1863875be2f856cd1515bf425cccb422c4ce2e
david78k <root@c11node28.(none)>
2014-02-19 00:37:28 -0500

    update

--- p2/src/enron/ken_subjects.sql
+++ p2/src/enron/ken_subjects.sql
@@ -2,25 +2,25 @@
 -- and insert query results into a new table and a local file
 -- and display the results and the count
 
-set tname=enron_steve;
-set tname_origin=enron;
+set tname=enron_ken_subjects;
+set tname_origin=enron_ken;
 
 -- create table 
 drop table ${hiveconf:tname};
 
 CREATE EXTERNAL TABLE ${hiveconf:tname} (
-  sender STRING,
-  recipient STRING,
-  cc STRING,
-  subject STRING,
-  context STRING
+  word STRING,
+  count INT
   );
 
--- insert the records of steve kean
+-- insert the records of ken lay
+-- order by frequency
 INSERT OVERWRITE TABLE ${hiveconf:tname}
-SELECT frome, toe, cc, subject, context
+SELECT trim(word), count(1) freq
 FROM ${hiveconf:tname_origin}
-WHERE frome LIKE '%steve%kean%'
+LATERAL VIEW explode(split(subject, ' +')) t AS word
+GROUP BY trim(word)
+ORDER BY freq DESC
 ;
 
 -- insert into a local file
@@ -31,5 +31,5 @@ FROM ${hiveconf:tname};
 
 select count(*) from ${hiveconf:tname};
 
-select * from ${hiveconf:tname} limit 5;
+select * from ${hiveconf:tname} limit 10;
 
