8cae75895fbd6fbd3a870c569e64cc27be015976
david78k <root@c11node28.(none)>
2014-04-07 17:10:21 -0400

    update

--- p3/Task2/src/reducer.cpp
+++ p3/Task2/src/reducer.cpp
@@ -66,6 +66,7 @@ void multiple(int argc, char **argv) {
 	
 	//cout << "line size = " << lines.size() << endl;
 	//cout << "Block size = " << blocksize << endl;
+	cout << "\nreduce" << endl;
 	reduce();
 	//writeFile();
 }
@@ -76,30 +77,30 @@ void reduce() {
 
 	//string line;
 	//char line[100];
-	//char data[100];
-	blocksize = 100001/nprocs;
+//	blocksize = 100001/nprocs;
 	//blocksize = 100000/nprocs;
-	int data[blocksize][2];
+	//int data[blocksize][2];
+	char data[100];
+	double b[128][32];
+	int i, j, blocksize, blocks;
 
-	//cout << "line size = " << lines.size() << endl;
-	//cout << "Block size = " << blocksize << endl;
-	//cout << "nprocs = " << nprocs << endl;
+	cout << "line size = " << lines.size() << endl;
+	cout << "nprocs = " << nprocs << endl;
 	//MPI_Recv(data, 100, MPI_CHAR, 0, 1, MPI_COMM_WORLD, NULL);
-	MPI_Recv(&data[0][0], blocksize*2, MPI_INT, 0, 1, MPI_COMM_WORLD, NULL);
+	//MPI_Recv(&data[0][0], blocksize*2, MPI_INT, 0, 1, MPI_COMM_WORLD, NULL);
+	MPI_Recv(&blocks, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, NULL);
+	cout << "Block size = " << blocks << endl;
+	MPI_Recv(&b[0][0], 128*32, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, NULL);
 	
-	cout << "Processor " << myrank << " received " << data << endl;
+	cout << "Processor " << myrank << " received " << b << endl;
+	//cout << "Processor " << myrank << " received " << data << endl;
 	//cout << "Processor " << myrank << " received " << line << endl;
-/*
-	MPI_Status status;
-
-	MPI_Request req_recv, req_send;
-
-//	MPI_Init(&argc, &argv);
-	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-	MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
-
-	MPI_Finalize();
-*/
+	
+	//for(int i = 0; i < blocksize; i ++) {
+	for(int i = 0; i < 10; i ++) {
+		//cout << data[i][0] << " " << data[i][1] << endl;
+		cout << b[i][0] << " " << b[i][1] << endl;
+	}
 }
 
 // assign lines to processors
@@ -111,7 +112,19 @@ void assign() {
 	int begin, end;
 	int N = lines.size();
 	blocksize = N/nprocs;
+	double a[128][32];
+	int i, j;
 
+	//MPI_Bcast(&blocksize, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	/* Generate the part of the matrix held by the current process
+        * so that each matrix element is unique   */
+ 	for (i = 0; i < 128; i++)
+        	for (j = 0; j < 32; j++)
+        	    a[i][j] = 1000 * i + j;
+        	    //a[i][j] = 1000 * i + j + 32 * rank;
+
+	cout << "Block size = " << blocksize << endl;
+	cout << "number of pairs = " << pairs.size() << endl;
 	for(int i = 0; i < nprocs; i++) {
 		begin = i * blocksize;
 		end = begin + blocksize - 1; 
@@ -126,10 +139,14 @@ void assign() {
 		char *cstr = new char[lines[i].length() + 1];
 		strcpy(cstr, lines[i].c_str());
 		cout << "MPI_send " << cstr << endl;
+		cout << "pairs[begin + 1][0]: "  << pairs[begin + 1][0] << endl;
 
+		int blocks = end - begin + 1;
+		MPI_Send(&blocks, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
+		MPI_Send(&a[0][0], 128*32, MPI_DOUBLE, i, 1, MPI_COMM_WORLD);
 		//MPI_Send(&pairs[begin][0], blocksize*2, MPI_INT, i, 1, MPI_COMM_WORLD);
 		//MPI_Send(cstr, strlen(cstr), MPI_CHAR, i, 1, MPI_COMM_WORLD);
-		MPI_Send(&lines[i], lines[i].size(), MPI_CHAR, i, 1, MPI_COMM_WORLD);
+		//MPI_Send(&lines[i], lines[i].size(), MPI_CHAR, i, 1, MPI_COMM_WORLD);
 	//	cout << "MPI message " << cstr << endl;
 	//	cout << cstr;
 	//	cout << " sent to processor " << i << endl;
@@ -171,11 +188,19 @@ void init(int argc, char **argv) {
 		vector<int> pair;
 		pair.push_back(key);
 		pair.push_back(value);
+		//cout << "pair: "  << pair[0] << ", " << pair[1] << endl;
 		pairs.push_back(pair);
 	}
 
 	cout << "number of lines = " << lines.size() << endl;
 	cout << "number of keys = " << table.size() << endl;
+	cout << "number of pairs = " << pairs.size() << endl;
+	cout << "lines[0]: "  << lines[0] << endl;
+	//cout << "pairs[1]: "  << pairs[1] << endl;
+	cout << "pairs[1][0]: "  << pairs[1][0] << endl;
+	cout << "pairs[1][1]: "  << pairs[1][1] << endl;
+	cout << "pairs[last][0]: "  << pairs[pairs.size()-1][0] << endl;
+	cout << "pairs[last][1]: "  << pairs[pairs.size()-1][1] << endl;
 	myfile.close();
   }
 
