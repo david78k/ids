44427ad08ba84e31916e803543b30a7706a2161a
david78k <root@c11node28.(none)>
2014-04-19 01:10:15 -0400

    update

--- p4/project4_dataset/jpgtocsv.m
+++ p4/project4_dataset/jpgtocsv.m
@@ -3,9 +3,12 @@ dirPath = [val '/Class'] ;
 outDirPath = ['csvfiles/' dirPath] ;
 nClasses = 6 ;
 for i = 1 : nClasses
-    dirPath1 = [dirPath,' ', num2str(i), '/'] ;
-    outDirPath1 = [outDirPath,' ', num2str(i), '/'] ;
-    mkdir(outDirPath1) ;
+    dirPath1 = [dirPath,' ', num2str(i), '/'] 
+    %outDirPath1 = [outDirPath,' ', num2str(i), '/'] 
+    outDirPath1 = [outDirPath, num2str(i), '/'] 
+    %mkdir(outDirPath1) 
+    cmd = ["mkdir -p " outDirPath1]
+    system(cmd) 
     F = dir(dirPath1) ;
     for j =  1 : length(F)-2
         imgName = F(j+2).name ;
@@ -13,11 +16,13 @@ for i = 1 : nClasses
         img = imread(fullImgName) ;
         [r, c, d] = size(img) ;
         imgReshaped = [img(:,:,1) ; img(:,:,2) ; img(:,:,3)] ;
-        splitImgName = regexp(imgName, '\.', 'split') ;
+        %splitImgName = regexp(imgName, '\.', 'split') ;
+        splitImgName = strsplit(imgName, '\.') ;
         imgNameWithoutExtension = splitImgName{1} ;
         fileName = [imgNameWithoutExtension '.csv'] ;
         fullFileName = [outDirPath1, fileName] ;
-        csvwrite(fullFileName, imgReshaped) ;
+        csvwrite(fullFileName, imgReshaped/16) ;
+        %csvwrite(fullFileName, imgReshaped) ;
     end
 end
 
